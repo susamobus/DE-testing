0ee3868b01624830d3eb87f7063013c308a2987c
Test <101502649+susamobus@users.noreply.github.com>
2023-04-23 15:12:21 +0800

    Return

--- update.js
+++ update.js
@@ -1,16 +1,6 @@
 var results = 0
-var results2 = 0
-var results3 = 0
-var results4 = 0
-var results5 = 0
-var re = 0
 window.setInterval(function(){
     document.getElementById("result").innerHTML = results
-    document.getElementById("k1").innerHTML = results2
-    document.getElementById("k2").innerHTML = results3
-    document.getElementById("k3").innerHTML = results4
-    document.getElementById("k4").innerHTML = results5
-    document.getElementById("i").innerHTML = re
 },50)
 
 function g(x,y) {
@@ -31,11 +21,6 @@ function RK(f, x0, y0, h, n) {
       const k2 = f(x + h / 2, y + h * k1 / 2);
       const k3 = f(x + h / 2, y + h * k2 / 2);
       const k4 = f(x + h, y + h * k3);
-      results2 = k1
-      results3 = k2
-      results4 = k3
-      results5 = k4
-      re = i
       y += h * (k1/6 + k2/3 + k3/3 + k4/6);
       x += h;
     }
