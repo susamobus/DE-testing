36fb2ebbd250311fd88df9d49f399bd4a778ec6b
Test <101502649+susamobus@users.noreply.github.com>
2023-04-23 14:36:10 +0800

    Fix

--- update.js
+++ update.js
@@ -1,12 +1,14 @@
 var results = 0
+var results2 = 0
+var results3 = 0
+var results4 = 0
+var results5 = 0
 window.setInterval(function(){
     document.getElementById("result").innerHTML = results
-    document.getElementById("k1").innerHTML = k1
-    document.getElementById("k2").innerHTML = k2
-    document.getElementById("k3").innerHTML = k3
-    document.getElementById("k4").innerHTML = k4
-    document.getElementById("i").innerHTML = i
-    document.getElementById("x").innerHTML = x
+    document.getElementById("k1").innerHTML = results2
+    document.getElementById("k2").innerHTML = results3
+    document.getElementById("k3").innerHTML = results4
+    document.getElementById("k4").innerHTML = results5
 },50)
 
 function g(x,y) {
@@ -27,10 +29,13 @@ function RK(f, x0, y0, h, n) {
       const k2 = f(x + h / 2, y + h * k1 / 2);
       const k3 = f(x + h / 2, y + h * k2 / 2);
       const k4 = f(x + h, y + h * k3);
-      results = k2
+      results2 = k1
+      results3 = k2
+      results4 = k3
+      results5 = k4
       y += h * (k1/6 + k2/3 + k3/3 + k4/6);
       x += h;
     }
-    // here
+    results = y
 }
   
