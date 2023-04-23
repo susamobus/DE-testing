b33d53a158f6a2b550135aad1dd93983bd40d1b2
Test <101502649+susamobus@users.noreply.github.com>
2023-04-23 15:24:43 +0800

    General

--- update.js
+++ update.js
@@ -1,4 +1,21 @@
 var results = 0
+var c2 = 1/2
+var c3 = 1/2
+var c4 = 1
+var b1 = 1/6
+var b2 = 1/3
+var b3 = 1/3
+var b4 = 1/6
+var a21 = 1/2
+var a31 = 0
+var a32 = 1/2
+var a41 = 0
+var a42 = 0
+var a43 = 1
+var c = [0,0,c2,c3,c4]
+var b = [0,b1,b2,b3,b4]
+var a = [0,0,[a21],[a31,a32],[a41,a42,a43]]
+
 window.setInterval(function(){
     document.getElementById("result").innerHTML = results
 },50)
@@ -18,10 +35,10 @@ function RK(f, x0, y0, h, n) {
     let y = y0;
     for (let i = 0; i < n; i++) {
       const k1 = f(x, y);
-      const k2 = f(x + h / 2, y + h * k1 / 2);
-      const k3 = f(x + h / 2, y + h * k2 / 2);
-      const k4 = f(x + h, y + h * k3);
-      y += h * (k1/6 + k2/3 + k3/3 + k4/6);
+      const k2 = f(x + c[2] * h, y + h * a[2][1] * k1);
+      const k3 = f(x + c[3] * h, y + h * (a[3][1] * k1 + a[3][2] * k2));
+      const k4 = f(x + c[4] * h, y + h * (a[4][1] * k1 + a[4][2] * k2 + a[4][3] * k3));
+      y += h * (b[1] * k1 + b[2] * k2 + b[3] * k3 + b[4] * k4);
       x += h;
     }
     results = y
