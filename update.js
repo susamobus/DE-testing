0b3c475830dd7061d11bbb641cbb614f829a3855
Test <101502649+susamobus@users.noreply.github.com>
2023-04-23 13:34:59 +0800

    Test 3

--- update.js
+++ update.js
@@ -4,7 +4,7 @@ window.setInterval(function(){
 },50)
 
 function g(x,y) {
-    return x^2;
+    return 2*x;
 }
 
 function Calculate() {
@@ -16,7 +16,7 @@ function RK(f, x0, y0, h, n) {
     // h is the step size, and n is the number of steps to take
     let x = x0;
     let y = y0;
-    for (let i = 1; i < 5; i++) {
+    for (let i = 1; i < n; i++) {
       const k1 = f(x, y);
       const k2 = f(x + h / 2, y + h * k1 / 2);
       const k3 = f(x + h / 2, y + h * k2 / 2);
