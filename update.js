559686a98935e875a337813b016a222c512cd55d
Test <101502649+susamobus@users.noreply.github.com>
2023-04-23 14:42:08 +0800

    Test

--- update.js
+++ update.js
@@ -3,12 +3,14 @@ var results2 = 0
 var results3 = 0
 var results4 = 0
 var results5 = 0
+var re = 0
 window.setInterval(function(){
     document.getElementById("result").innerHTML = results
     document.getElementById("k1").innerHTML = results2
     document.getElementById("k2").innerHTML = results3
     document.getElementById("k3").innerHTML = results4
     document.getElementById("k4").innerHTML = results5
+    document.getElementById("i").innerHTML = re
 },50)
 
 function g(x,y) {
@@ -25,7 +27,7 @@ function RK(f, x0, y0, h, n) {
     let x = x0;
     let y = y0;
     for (let i = 0; i < n; i++) {
-      const k1 = g(x, y);
+      const k1 = f(x, y);
       const k2 = f(x + h / 2, y + h * k1 / 2);
       const k3 = f(x + h / 2, y + h * k2 / 2);
       const k4 = f(x + h, y + h * k3);
@@ -33,6 +35,7 @@ function RK(f, x0, y0, h, n) {
       results3 = k2
       results4 = k3
       results5 = k4
+      re = i
       y += h * (k1/6 + k2/3 + k3/3 + k4/6);
       x += h;
     }
