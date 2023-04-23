bbc82b4ba3e7700990c9af98af2c5713edea67ef
Test <101502649+susamobus@users.noreply.github.com>
2023-04-23 13:22:24 +0800

    Fix 5

--- update.js
+++ update.js
@@ -21,8 +21,8 @@ function RK(f, x0, y0, h, n) {
       const k2 = f(x + h / 2, y + h * k1 / 2);
       const k3 = f(x + h / 2, y + h * k2 / 2);
       const k4 = f(x + h, y + h * k3);
-      y += h * (k1/6 + k2/3 + k3/3 + k4/6);
-      x += h;
+      y = y + h * (k1/6 + k2/3 + k3/3 + k4/6);
+      x = x + h;
     }
     results = y
 }
