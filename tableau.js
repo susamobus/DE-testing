6662a16c13efcedda0f55eb30a08e3545cfae0c9
Test <101502649+susamobus@users.noreply.github.com>
2023-04-24 07:27:21 +0800

    Fix

--- tableau.js
+++ tableau.js
@@ -24,9 +24,11 @@ function CreateTableau() {
               input.className = "TableInput";
               if (j == 1) {
                 input.id = "TableInputC";
+                input.name = "c" + i;
                 input.placeholder = "c" + i;
               } else {
                 input.id = "TableInputA";
+                input.name = "a" + i + "," + j
                 input.placeholder = "a" + i + "," + j;
               }
               cell.appendChild(input)
@@ -47,17 +49,37 @@ function CreateTableau() {
         cell.appendChild(input)
         row.appendChild(cell)
 
-        for (let i = 0; i < s; i++) {
+        for (let i = 1; i <= s; i++) {
           let cell = document.createElement("td")
           let input = document.createElement("input");
           input.type = "text";
           input.className = "TableInput";
-          input.id = "TableInputB"
-          input.placeholder = "b" + (i + 1);
+          input.id = "TableInputB";
+          input.name = "b" + i;
+          input.placeholder = "b" + i;
           cell.appendChild(input);
           row.appendChild(cell);
         }
         tfoot.appendChild(row)
         table.appendChild(tfoot)
 
+}
+
+function UpdateTableau() {
+  a = [0,0];
+  b = [0];
+  c = [0,0];
+  for (i = 1; i <= s; i++) {
+    b.push(0)
+    if (i > 1) {
+      a.push(0)
+      c.push(0)
+      for (j = 1; j < s; j++) {
+        a[i].push(0)
+        a[i][j] = document.getElementsByName("a" + i + "," + j)[0].value
+      }
+      c[i] = document.getElementsByName("c" + i)[0].value
+    }
+    b[i] = document.getElementsByName("b" + i)[0].value
+  }
 }
