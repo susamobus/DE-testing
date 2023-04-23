c609a4ef8207d65b195a106b0b418a766bcc8934
Test <101502649+susamobus@users.noreply.github.com>
2023-04-23 21:41:12 +0800

    Fix

--- tableau.js
+++ tableau.js
@@ -9,7 +9,7 @@ function CreateTableau() {
         // Create input rows for coefficients
         for (let i = 1; i <= s; i++) {
           let row = document.createElement("tr");
-          for (let j = 0; j <= i; j++) {
+          for (let j = 1; j < i; j++) {
             let cell = document.createElement("td");
             if (i == 0 && j == 0) {
               let input = document.createElement("input");
